10eb72a9c24bf0ae78bce22277dde55ada9818fa
sxoxgxi <sxoxgxi@duck.com>
2026-03-09 16:19:10 +0545

    remove: duplicate code

--- script.js
+++ script.js
@@ -393,10 +393,3 @@ setInterval(updateGreeting, 60000);
 loadEngines();
 loadLinks();
 input.focus();
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(() => console.log("SW registered"))
-    .catch(console.error);
-}
